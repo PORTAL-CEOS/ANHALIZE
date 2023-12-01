83fbb04a41cf3dff2ef2968dcbf82d1f147cff5b
Luiz Henrique da Silva <dasilvlh@myumanitoba.ca>
2023-12-01 13:28:10 -0300

    Fix mask creator. Change the interactive mode from qT to Tk. Change the polygon fill method from openCV to use Image/Imagedraw instead.

--- anhalyze/create_mask_region.py
+++ anhalyze/create_mask_region.py
@@ -13,14 +13,13 @@ Interactively creates a Mask file drawing a polygon over the ANHA4 domain.
 The user can zoom in in the figure to get a more accurate polygon.
 
 """
-
+import matplotlib
+matplotlib.use('TkAgg')
 import numpy as np
 import netCDF4 as nc
-import cv2OpenCV
-
+from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 from matplotlib.widgets import PolygonSelector
-
 
 # %%
 # Use the ANHALYZE get_paths
@@ -36,38 +35,46 @@ tmask_anha4 = mask_anha4['tmask'][0][0]
 # %%
 # Create polygon
 
-fig, ax = plt.subplots()
-ax.contourf(tmask_anha4)
-fig.show()
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.pcolormesh(tmask_anha4)
 
-def onselect(vertices):
+
+def onselect(verts):
     pass
 
+# PolygonSelector open a window using a previous figure 
+# so the user can draw the vertices around the region of interest.
 selector = PolygonSelector(ax, onselect)
 
 print("Click on the figure to create a polygon.")
 print("Press the 'esc' key to start a new polygon.")
-
+plt.show(block=True)
 # %%
 # Set everything out of the polygon as zeros
-
-# New variable with the vertices indices. Rounded because the
-# PolygonSelector returns float numbers.  
+    # New variable with the vertices indices. Rounded because the
+    # PolygonSelector returns float numbers.  
+vert = None
 vert = np.array([np.intc(np.round(vertices,0)) for vertices in selector.verts],'int32')
+vert = [tuple(x) for x in vert]
 
 # Create a new numpy array with the ANHA4 mask dimensions
 mask_region = np.zeros(tmask_anha4.shape,dtype=np.uint8)
 
-# Set every value within the polygon as 1
-cv2.fillConvexPoly(mask_region,vert,1) 
+# Mask dimensions
+x = mask_region.shape[1]
+y = mask_region.shape[0]
 
+# Image and ImageDraw.Draw().Polygon to fill up the polygon area with ones
+img = Image.new('L', (x, y), 0)
+ImageDraw.Draw(img).polygon(vert, fill=1)
+
+# Convert the image object to a numpy object
+mask_region = np.array(img,dtype='int32') 
+    
 # Multiply the masked region by the ANHA4 mask so we get back the land within 
 # the polygon area
 mask = mask_region*tmask_anha4
-
-# Visualize the mask
-plt.pcolor(mask)
-
 # %%
 # SAVE MASK AS NETCDF4 
 try: ncfile.close()
@@ -76,8 +83,8 @@ except: pass
 ncfile = nc.Dataset('/home/camus/ANHALIZE/new_mask.nc',mode = 'w', format='NETCDF4_CLASSIC')
 
 # Creating dimensions
-y_dim = ncfile.createDimension('y', mask.shape[0])     # latitude axis
-x_dim = ncfile.createDimension('x', mask.shape[1])    # longitude axis
+y_dim = ncfile.createDimension('y', y)     # latitude axis
+x_dim = ncfile.createDimension('x', x)    # longitude axis
 time_dim = ncfile.createDimension('time', None) # unlimited axis (can be appended to).
     
 # Title and history
@@ -95,4 +102,3 @@ tmask[:,:] = mask
 ncfile.close();
 
 # %%
-!ncdump -h /home/camus/ANHALIZE/new_mask.nc
